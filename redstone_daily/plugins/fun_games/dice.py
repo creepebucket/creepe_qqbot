6f13dfaf1f81cac7e000893739a286d2bcb90da9
creepebucket <creepebucket@qq.com>
2025-08-31 02:11:55 +0800

    重构

--- redstone_daily/plugins/fun_games/dice.py
+++ redstone_daily/plugins/fun_games/dice.py
@@ -12,16 +12,15 @@ add_info('dice', '掷骰子, 用法: /xxxdyyy, x为个数, y为面数')
 
 @check_command_enabled('dice')
 async def dice_handler(event: Event):
-    message = event.get_message().extract_plain_text()
-    print(message)
+    message = event.get_message().extract_plain_text().strip()
+
     if not re.match('^/[0-9]{1,3}d[0-9]{1,3}$'):
         return
-    print(1)
 
     args = message.replace('/', '').split('d')
     answer = '['
 
     for i in range(int(args[0])):
-        answer += f' {random.randint(1, args[2])} '
+        answer += f' {random.randint(1, int(args[1]))} '
 
     await dice.send(f'{answer}]')
